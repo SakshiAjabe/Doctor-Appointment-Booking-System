6492dd4f26e81cfb84d204ff92ede79456398087
Sakshi Ajabe <sakshiajabe4@gmail.com>
2025-01-08 22:31:34 +0530

    update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,25 +3,25 @@ import { assets } from '../assets/assets'
 
 const Header = () => {
   return (
-    <div>
+    <div className='flex flex-col flex-wrap px-6 rounded-lg md:flex-row bg-primary md:px-10 lg:px-20'>
 
         {/* left side */}
-        <div>
-            <p>
+        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb[-30px]'>
+            <p className='text-3xl font-semibold leading-tight text-white md:text-5xl md:leading-tight lg:leading'>
                 Book Appointment <br /> With Trusted Doctors
             </p>
-            <div>
-                <img src={assets.group_profiles} alt="" />
-                <p>simply browse through our extensive list of trusted doctors , <br /> schedule your appointment hassle-free</p>
+            <div className='flex flex-col items-center gap-3 text-sm font-light text-white md:flex-row'>
+                <img className='w-28'  src={assets.group_profiles} alt="" />
+                <p>simply browse through our extensive list of trusted doctors , <br className='hidden sm:block' /> schedule your appointment hassle-free</p>
             </div>
-            <a href="">
-                Book appointment <img src={assets.arrow_icon} alt="" />
+            <a href="#speciality" className='flex items-center gap-2 px-8 py-3 m-auto text-sm text-gray-600 transition-all duration-300 bg-white rounded-full md:m-0 hover:scale-105'>
+                Book appointment <img className='w-3' src={assets.arrow_icon} alt="" />
             </a>
         </div>
 
         {/* right side */}
-        <div>
-            <img src={assets.header_img} alt="" />
+        <div className='relative md:w-1/2'>
+            <img className='bottom-0 w-full h-auto rounded-lg md:absolute' src={assets.header_img} alt="" />
         </div> 
     </div>
   )
