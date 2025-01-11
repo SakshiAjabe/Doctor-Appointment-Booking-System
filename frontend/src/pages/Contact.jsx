c269420e1cef3a8bf586229d464f57e21a5fec4a
Sakshi Ajabe <sakshiajabe4@gmail.com>
2025-01-12 00:13:39 +0530

    update

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -11,13 +11,14 @@ const Contact = () => {
 
       <div className='flex flex-col justify-center gap-10 my-10 text-sm md:flex-row mb-28'>
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+
         <div className='flex flex-col items-start justify-center gap-6'>
-          <p>OUR OFFICE</p>
-          <p>54708 Willms Station <br /> Suite 350 , Washington, USA</p>
-          <p>Tel: (415) 555-0132 <br />Email: prescripto@gamil.com</p>
-          <p>Careers At PRESCRIPTO</p>
-          <p>Learn more about our teams and join job openings.</p>
-          <button>Explore Jobs</button>
+          <p className="text-lg font-semibold text-gray-600">OUR OFFICE</p>
+          <p className='text-gray-500'>54708 Willms Station <br /> Suite 350 , Washington, USA</p>
+          <p className='text-gray-500'>Tel: (415) 555-0132 <br />Email: prescripto@gamil.com</p>
+          <p className='text-lg font-semibold text-gray-600'>Careers At PRESCRIPTO</p>
+          <p className='text-gray-500'>Learn more about our teams and join job openings.</p>
+          <button className='px-8 py-4 text-sm transition-all duration-500 border border-black hover:bg-black hover:text-white'>Explore Jobs</button>
         </div>
       </div>
     </div>
--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
 
 const MyProfile = () => {
+
+  const [userData ,setUserData] = useState({
+    name:"edward vincent",
+    image:assets.profile_pic,
+    email:'richardjameswap@gamil.com',
+    phone:'+1 123 456 7890',
+    address: {
+      line1:"57th Cross, Richmond",
+      line2:"Circle, Chrurch Road, London"
+    },
+
+    gender:"Male",
+    dob: '2000-01-20'
+
+  })
+
+  const [isEdit , setIsEdit] = useState(false)
+
   return (
     <div>
       
+      <img src={userData.image} alt="" />
+
+      {
+        isEdit
+        ? <input type="text" />
+        : <p>{userData.name}</p>
+      }
+
     </div>
   )
 }
